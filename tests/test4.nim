048497a0d290143195702b17ab69caac61224828
sls1005 <710052@mail.pcsh.ntpc.edu.tw>
2022-11-25 22:08:18 +0800

    Update the test

--- tests/test4.nim
+++ tests/test4.nim
@@ -1,6 +1,11 @@
 from stdarg/wchar import CWideChar
-import unicode
 
-var x: CWideChar
-x = cast[CWideChar]('A')
-echo x
+proc printf(f: cstring): cint {.importc, varargs, header: "<stdio.h>".}
+
+proc main =
+  var x: CWideChar
+  x = cast[CWideChar]('A')
+  if printf("%lc\n", x) < 0:
+    raise newException(ValueError, "")
+
+main()
