7d3318a7b3e7577d8800b10da64c5b51fd0b3efe
sls1005 <710052@mail.pcsh.ntpc.edu.tw>
2022-11-26 18:41:56 +0800

    update the test

--- tests/test4.nim
+++ tests/test4.nim
@@ -4,7 +4,7 @@ proc printf(f: cstring): cint {.importc, varargs, header: "<stdio.h>".}
 
 proc main =
   var x: CWideChar
-  x = cast[CWideChar]('A')
+  x = CWideChar('A')
   if printf("%lc\n", x) < 0:
     raise newException(ValueError, "")
 
